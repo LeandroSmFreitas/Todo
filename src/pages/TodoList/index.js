e3a6b68b95e314dca9523671acb868d40d84e017
Leandro Simoes <leandrosimoes42@gmail.com>
2022-01-18 10:59:05 -0300

    ✨ Feat: creating the logic of creating tasks, ending tasks and reversing the ending of the task

--- src/pages/TodoList/index.js
+++ src/pages/TodoList/index.js
@@ -5,20 +5,62 @@ import { DbContext } from '../../Provider/DbProvider'
 import { Container, ContainerAddTasks, ContainerAllTitleOfImportance, ContainerTasks, ContainerTitleTask, SelectStyled } from './style'
 import { MdControlPoint } from "react-icons/md"
 import Button from '../../components/Button'
+import * as yup from 'yup';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import CardOfTask from '../../components/cardTask'
+import * as uuid from 'uuid'
 
 export default function TodoList() {
     const [valueSelect, setValueSelect] = useState('')
+    const {token, GetToken } = useContext(DbContext)
+    const [userTasks, setUserTasks] = useState([])
+    const [userTasksFinished, setUserTasksFinished] = useState([])
+    
+    const schema = yup.object().shape({
+        task: yup.string().required("Campo obrigatorio!"),
+        importance: yup.string().oneOf(['Urgente', 'Prioridade', 'Importante', 'Não importante'])
+    });
+
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema),
+    });
+
+    if(!token){
+        GetToken()
+    }
     
     const onSubmitForm = (data) => {
-
+        const task = {
+            id: uuid.v4(),
+            name: data.task,
+            importance: data.importance,
+            user: token,
+        }
+        setUserTasks([...userTasks, task])
     }
+
+    const FinishedTask = (id) => {
+        console.log(userTasks)
+        let task = userTasks.find(task => task.id = id)
+        setUserTasks(userTasks.filter(task => task.id !== id))
+        setUserTasksFinished([...userTasksFinished, task])
+    }
+
+    const RevertFinishedTask = (id) => {
+        let task = userTasksFinished.find(task => task.id = id)
+        setUserTasksFinished(userTasksFinished.filter(task => task.id !== id))
+        setUserTasks([...userTasks, task])
+    }
+
+
     
     return (
         <Container>
             <h1>O seu espaço de gerenciamento de tarefas!</h1>
-            <ContainerAddTasks>
-                <Input Icon={MdControlPoint} borderRadius='20px 0 0 20px' widthContainer='70%' heightContainer='30px' placeHolder='adicionar tarefa' border='1px solid'/>
-                <SelectStyled onChange={(e) => setValueSelect(e.currentTarget.value)} value={valueSelect}>
+            <ContainerAddTasks onSubmit={handleSubmit(onSubmitForm)}>
+                <Input Icon={MdControlPoint} borderRadius='20px 0 0 20px' widthContainer='70%' heightContainer='30px' placeHolder='adicionar tarefa' border='1px solid' register={register} name='task'/>
+                <SelectStyled {...register('importance')} onChange={(e) => setValueSelect(e.currentTarget.value)} value={valueSelect}>
                     <option value="">Nivel de Urgência</option>
                     <option value="Urgente">Urgente</option>
                     <option value="Prioridade">Prioridade</option>
@@ -32,21 +74,49 @@ export default function TodoList() {
                     <ContainerTitleTask>
                         <p>Urgente</p>
                     </ContainerTitleTask>
+                    {userTasks.map((element) => {
+                        if(element.importance === 'Urgente'){
+                            return <CardOfTask key={element.id} title={element.name} click={() => FinishedTask(element.id)}/>
+                        }
+                    })}
                 </ContainerTasks>
                 <ContainerTasks>
                     <ContainerTitleTask>
                         <p>Prioridade</p>
                     </ContainerTitleTask>
+                    {userTasks.map((element) => {
+                        if(element.importance === 'Prioridade'){
+                            return <CardOfTask key={element.id} title={element.name} click={() => FinishedTask(element.id)}/>
+                        }
+                    })}
                 </ContainerTasks>
                 <ContainerTasks>
                     <ContainerTitleTask>
                         <p>Importante</p>
                     </ContainerTitleTask>
+                    {userTasks.map((element) => {
+                        if(element.importance === 'Importante'){
+                            return <CardOfTask key={element.id} title={element.name} click={() => FinishedTask(element.id)}/>
+                        }
+                    })}
                 </ContainerTasks>
                 <ContainerTasks>
                     <ContainerTitleTask>
                         <p>Não importante</p>
                     </ContainerTitleTask>
+                    {userTasks.map((element) => {
+                        if(element.importance === 'Não importante'){
+                            return <CardOfTask key={element.id} title={element.name} click={() => FinishedTask(element.id)}/>
+                        }
+                    })}
+                </ContainerTasks>
+                <ContainerTasks>
+                    <ContainerTitleTask>
+                        <p>Finalizadas</p>
+                    </ContainerTitleTask>
+                    {userTasksFinished.map((element) => {
+                        return <CardOfTask key={element.id} title={element.name} click={() => RevertFinishedTask(element.id)} taskFinished={true}/>
+                    })}
                 </ContainerTasks>
             </ContainerAllTitleOfImportance>
         </Container>
